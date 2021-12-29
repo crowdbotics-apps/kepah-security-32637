0f05f4cac8b0dbf4c845a4857e9abb0306920806
Crowdbotics <team@crowdbotics.com>
2021-12-29 19:10:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "kepah_security_32637",
+  url: "https://kepah_security_32637.botics.co",
+  api: "https://kepah_security_32637.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
